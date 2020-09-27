9dac093075b051736823c589d20adbb5f66d86ba
baizhuangzhuang <baizhuangzhuang>
2020-09-27 18:02:40 +0800

    Initial commit

---
+++ src/main/java/top/strategy/impl/OperationAdd.java
@@ -0,0 +1,8 @@
+package top.strategy.impl;
+
+/**
+ * @author bz
+ * @date 2020/9/27
+ */
+public class OperationAdd {
+}
---
+++ src/main/java/top/strategy/parent/Strategy.java
@@ -0,0 +1,8 @@
+package top.strategy.parent;
+
+/**
+ * @author bz
+ * @date 2020/9/27
+ */
+public interface Strategy {
+}
